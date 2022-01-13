1892cf1b8b683e84b70c50e0b849f858c5680d0b
nanaakuffu <nakuffu@aim.ac.tz>
2022-01-13 12:03:21 +0000

    Added DB module to the main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { FeeModule } from './fees/modules/fee.module';
 import LoggerMiddleware from './middlewares/logger.middleware';
+import { DBModule } from './db/module/db.module';
 
 @Module({
-  imports: [FeeModule],
+  imports: [FeeModule, DBModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('fees');
+    consumer.apply(LoggerMiddleware).forRoutes('*');
   }
 }
