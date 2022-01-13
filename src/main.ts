5500a92e27b64847910c80302b49c32f9e6916d3
nanaakuffu <nakuffu@aim.ac.tz>
2022-01-13 12:04:39 +0000

    Made room for prisma destroy event in main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import DBService from './db/db.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/v1/api');
+
+  const dbService: DBService = app.get(DBService);
+  dbService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
