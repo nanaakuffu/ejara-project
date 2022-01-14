6c03359f6bcae091f096ef90dbbb5e60a8150f04
nanaakuffu <nakuffu@aim.ac.tz>
2022-01-14 19:16:08 +0000

    Added HttpModule to FeeModule to use HttpService

--- src/fees/modules/fee.module.ts
+++ src/fees/modules/fee.module.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { FeeController } from './../fees.controller';
 import { FeeService } from './../fees.service';
@@ -5,5 +6,6 @@ import { FeeService } from './../fees.service';
 @Module({
   controllers: [FeeController],
   providers: [FeeService],
+  imports: [HttpModule],
 })
 export class FeeModule {}
