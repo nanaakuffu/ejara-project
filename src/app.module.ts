2ae7ce08cdb903443ba1872c33e4ce7cd70756ae
nanaakuffu <nakuffu@aim.ac.tz>
2022-01-16 01:52:38 +0000

    Added configure module to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import LoggerMiddleware from './middlewares/logger.middleware';
 import { DBModule } from './db/module/db.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FeeModule, DBModule, AuthModule, UsersModule],
+  imports: [
+    FeeModule,
+    DBModule,
+    AuthModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
