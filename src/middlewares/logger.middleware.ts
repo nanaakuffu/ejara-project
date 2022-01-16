7a8a61a9e79e3841c00d420705b2f2169a52feb8
nanaakuffu <nakuffu@aim.ac.tz>
2022-01-16 01:58:57 +0000

    Decorated the custom logging for the system

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -4,7 +4,9 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export default class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('Request ...');
+    console.log(
+      `Request ... ${req.baseUrl} ... ${res.statusCode} ... ${req.statusMessage}`,
+    );
     next();
   }
 }
