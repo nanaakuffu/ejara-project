9959d39587a0fbb2f282991a96ab48926c1f7708
nanaakuffu <nakuffu@aim.ac.tz>
2022-01-16 03:22:24 +0000

    Increased token longevity to 1 hour

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import JwtStrategy from './jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '3600s' },
+      signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
