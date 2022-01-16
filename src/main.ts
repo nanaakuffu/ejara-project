456dcaca46e53f911e36d60b8db3234b01622077
nanaakuffu <nakuffu@aim.ac.tz>
2022-01-16 01:53:21 +0000

    Enabled swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import DBService from './db/db.service';
 
@@ -6,9 +8,25 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/v1/api');
 
+  const PORT = process.env.PORT;
+
+  const config = new DocumentBuilder()
+    .setTitle('Ejara Solution')
+    .setDescription('The fee API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('Ejara Test')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  // app.use(helmet());
+  app.enableCors();
+
   const dbService: DBService = app.get(DBService);
   dbService.enableShutdownHooks(app);
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
